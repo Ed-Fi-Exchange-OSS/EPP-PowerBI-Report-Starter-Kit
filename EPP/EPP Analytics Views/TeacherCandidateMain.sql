15058996e505eaec3843a93c97ff9b99520d7583
rmosman <73557889+rmosman@users.noreply.github.com>
2020-12-23 11:51:17 -0600

    Add files via upload

---
+++ EPP/EPP Analytics Views/TeacherCandidateMain.sql
@@ -0,0 +1,42 @@
+USE [EdFi_Ods_Populated_Template3]
+GO
+
+/****** Object:  View [analytics].[TeacherCandidateMain]    Script Date: 12/21/2020 12:47:56 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('[analytics].[TeacherCandidateMain]') IS NOT NULL 
+	DROP VIEW [analytics].[TeacherCandidateMain]
+
+GO
+
+
+CREATE VIEW [analytics].[TeacherCandidateMain] AS
+
+	SELECT tc.TeacherCandidateIdentifier
+		,tc.FirstName
+		,tc.LastSurname
+		,tc.SexDescriptorId
+		,r.RaceDescriptorId
+		,tc.HispanicLatinoEthnicity
+		,tc.EconomicDisadvantaged
+		,c.SchoolYear as Cohort
+		,tc.ProgramComplete
+		,tc.StudentUSI
+		,tpp.ProgramName
+		,tpp.BeginDate
+		,tpp.EducationOrganizationId
+		,tc.PersonId
+		,cred.Credentialed
+	FROM [tpdm].[TeacherCandidate] tc
+	JOIN tpdm.TeacherCandidateTeacherPreparationProviderProgramAssociation tpp on tpp.TeacherCandidateIdentifier = tc.TeacherCandidateIdentifier
+	LEFT OUTER JOIN dbo.[tpdm.tcrace] r on tc.TeacherCandidateIdentifier = r.TeacherCandidateIdentifier
+	LEFT OUTER JOIN edfi.Descriptor rd on r.RaceDescriptorId = rd.DescriptorId
+	LEFT OUTER JOIN tpdm.TeacherCandidateCohortYear c on tc.TeacherCandidateIdentifier = c.TeacherCandidateIdentifier
+	LEFT OUTER JOIN dbo.Credentialed cred on tc.TeacherCandidateIdentifier = cred.TeacherCandidateIdentifier
+GO
+
+
