d7d3cd93121bfd76dec6d2c508616b620bb81073
rmosman <73557889+rmosman@users.noreply.github.com>
2021-02-10 08:59:50 -0600

    Update EvaluationElementRating.sql

--- EPP/EPP Analytics Views/EvaluationElementRating.sql
+++ EPP/EPP Analytics Views/EvaluationElementRating.sql
@@ -1,19 +1,19 @@
 USE [EdFi_Ods_Populated_Template3]
 GO
 
-/****** Object:  View [analytics].[EvaluationElementRating]    Script Date: 12/21/2020 1:01:51 PM ******/
+/****** Object:  View [analytics].[tpdm_EvaluationElementRating]    Script Date: 12/21/2020 1:01:51 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-IF OBJECT_ID('[analytics].[EvaluationElementRating]') IS NOT NULL
-	DROP VIEW [analytics].[EvaluationElementRating]
+IF OBJECT_ID('[analytics].[tpdm_EvaluationElementRating]') IS NOT NULL
+	DROP VIEW [analytics].[tpdm_EvaluationElementRating]
 
 GO
 
-CREATE VIEW [analytics].[EvaluationElementRating] AS
+CREATE VIEW [analytics].[tpdm_EvaluationElementRating] AS
 
 ---Evaluation Rating: perfomance evaluation >> Objective >> Element
 SELECT DISTINCT tc.TeacherCandidateIdentifier
