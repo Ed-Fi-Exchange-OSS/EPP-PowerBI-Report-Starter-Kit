a4cd45dd34239cca77ed818a772683e64613788b
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2021-07-01 20:32:27 +0530

    deleted swagger js file

--- packer/scripts/installers/swagger_index.js
+++
@@ -1,5 +0,0 @@
-.then(() => {
-    var year = new Date().getFullYear()
-    $("#schoolYearSelect option[value='" + year + "']").removeAttr("selected")
-    $("#schoolYearSelect option[value='" + (year + 1) + "']").attr("selected", "selected")
-  })
