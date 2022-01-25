d95fad2132ae4db754531940c86e96e3a6c4bdf4
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-01-25 10:21:50 -0600

    Includes cohort year term descriptor.

--- Starter Kit Support/Analytics Views/TeacherCandidateMain.sql
+++ Starter Kit Support/Analytics Views/TeacherCandidateMain.sql
@@ -32,15 +32,18 @@ SELECT c.CandidateIdentifier
 		,epp.EducationOrganizationId 
 		,c.PersonId 
 		,CASE WHEN SUM(CASE WHEN cred.CredentialIdentifier IS NOT NULL THEN 1 ELSE 0 END) > 0 THEN MIN(cred.IssuanceDate) END IssuanceDate
+        ,termdesc.Description CohortYearTermDescription
 	FROM tpdm.Candidate c 
 	JOIN tpdm.CandidateEducatorPreparationProgramAssociation epp on epp.CandidateIdentifier = c.CandidateIdentifier 
 	LEFT JOIN tpdm.CandidateRace rd on rd.CandidateIdentifier = c.CandidateIdentifier 
 	LEFT JOIN edfi.Descriptor d on d.DescriptorId = rd.RaceDescriptorId 
 	LEFT JOIN edfi.Student s on s.PersonId = c.PersonId 
 	LEFT JOIN tpdm.CandidateEducatorPreparationProgramAssociationCohortYear ccy on ccy.CandidateIdentifier = c.CandidateIdentifier
+    LEFT JOIN edfi.Descriptor termdesc on ccy.TermDescriptorId = termdesc.DescriptorId
 	LEFT JOIN tpdm.CredentialExtension ce on ce.PersonId = c.PersonId 
 	LEFT JOIN edfi.Credential cred on cred.CredentialIdentifier = ce.CredentialIdentifier
 	LEFT JOIN edfi.Descriptor red ON epp.ReasonExitedDescriptorId = red.DescriptorId
+    
 	GROUP BY c.CandidateIdentifier 
 		,c.FirstName 
 		,c.LastSurname 
@@ -55,5 +58,5 @@ SELECT c.CandidateIdentifier
 		,epp.BeginDate 
 		,epp.EducationOrganizationId 
 		,c.PersonId
-
+        ,termdesc.Description
 GO
