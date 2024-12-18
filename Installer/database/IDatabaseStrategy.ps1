0fbb23d9001886e7fdf82e06da07fddfee8ea14b
Stephen A. Fuqua <stephen@safnet.com>
2024-12-18 15:38:29 -0600

    Delete duplicate installer information

--- Installer/database/IDatabaseStrategy.ps1
+++
@@ -1,6 +0,0 @@
-class IDatabaseStrategy {
-    [string] Get_ArtifactsFolder(){ return ""}
-    [string] Get_SchemaScript([string]$history){ return "" }
-    [string] Get_HistoryTableScript(){ return "" }
-    [string] Get_HistoryInsertScript($ScriptName){ return "" }
-}
